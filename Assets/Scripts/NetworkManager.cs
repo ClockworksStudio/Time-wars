5e05c53d5ea47505630cd6a49c796726fa8b0a80
Matias Jensen <main@clockworksstudio.com>
2014-11-11 17:42:10 +0100

    Connection can be completed, but transferred positions are acting weird.

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	public GameObject playerPrefab;
+	public string roomName = "Server Name:";
+
+	private RoomInfo[] roomsList;
+
+	// Use this for initialization
+	void Start ()
+	{
+		PhotonNetwork.ConnectUsingSettings("0.0.2");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnGUI()
+	{
+		if (!PhotonNetwork.connected)
+		{
+			GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+		}
+		else if (PhotonNetwork.room == null)
+		{
+			GUI.Label(new Rect(100, 50, 250, 20), "Server Name:");
+			roomName = GUI.TextField(new Rect(100, 70, 250, 20), roomName, 25);
+			// Create Room
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+			{
+				PhotonNetwork.CreateRoom(roomName, true, true, 5);
+			}
+			
+			// Join Room
+			if (roomsList != null)
+			{
+				for (int i = 0; i < roomsList.Length; i++)
+				{
+					if (GUI.Button(new Rect(100, 250 + (110 * i), 250, 100), "Join " + roomsList[i].name))
+					{
+						PhotonNetwork.JoinRoom(roomsList[i].name);
+					}
+				}
+			}
+		}
+	}
+	
+	void OnReceivedRoomListUpdate()
+	{
+		roomsList = PhotonNetwork.GetRoomList();
+	}
+	void OnJoinedRoom()
+	{
+		Debug.Log("Connected to Room!");
+
+		// Spawn player
+		PhotonNetwork.Instantiate(playerPrefab.name, Vector3.up * 5, Quaternion.identity, 0);
+	}
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : Photon.MonoBehaviour
+{
+	public float speed = 10f;
+	public bool debug = false;
+
+	private float lastSynchronizationTime = 0f;
+	private float syncDelay = 0f;
+	private float syncTime = 0f;
+	private Vector3 syncStartPosition = Vector3.zero;
+	private Vector3 syncEndPosition = Vector3.zero;
+	
+	void Update()
+	{
+		if (photonView.isMine)
+		{
+			InputMovement();
+		}
+		else
+		{
+			SyncedMovement();
+		}
+	}
+	
+	void InputMovement()
+	{
+		if (Input.GetKey(KeyCode.W))
+		{
+			rigidbody.MovePosition(rigidbody.position + Vector3.forward * speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.S))
+		{
+			rigidbody.MovePosition(rigidbody.position - Vector3.forward * speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			rigidbody.MovePosition(rigidbody.position + Vector3.right * speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			rigidbody.MovePosition(rigidbody.position - Vector3.right * speed * Time.deltaTime);
+		}
+
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			if(debug == true)
+			{
+				debug = false;
+			}
+			else
+			{
+				debug = true;
+			}
+		}
+	}
+
+	private void SyncedMovement()
+	{
+		syncTime += Time.deltaTime;
+		rigidbody.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
+	}
+
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			stream.SendNext(rigidbody.position);
+			stream.SendNext(rigidbody.velocity);
+		}
+		else
+		{
+			Vector3 syncPosition = (Vector3)stream.ReceiveNext();
+			Vector3 syncVelocity = (Vector3)stream.ReceiveNext();
+			
+			syncTime = 0f;
+			syncDelay = Time.time - lastSynchronizationTime;
+			lastSynchronizationTime = Time.time;
+
+			syncEndPosition = syncPosition + syncVelocity * syncDelay;
+			syncStartPosition = rigidbody.position;
+		}
+	}
+
+	void OnGUI()
+	{
+		if(debug == true)
+		{
+			GUI.Label(new Rect(30, 20, 250, 20), "Debug: deltaTime: "+Time.deltaTime.ToString());
+			GUI.Label(new Rect(30, 35, 250, 20), "Debug: syncTime: "+syncTime.ToString());
+			GUI.Label(new Rect(30, 50, 250, 20), "Debug: syncDelay: "+syncDelay.ToString());
+			GUI.Label(new Rect(30, 65, 250, 20), "Debug: syncStartPosition: "+syncEndPosition.ToString());
+			GUI.Label(new Rect(30, 80, 250, 20), "Debug: syncEndPosition: "+syncStartPosition.ToString());
+			GUI.Label(new Rect(30, 95, 250, 20), "Debug: transformPosition: "+transform.position.ToString());
+		}
+	}
+}
